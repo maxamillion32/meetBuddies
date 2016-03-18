2bdcf975ef61f1f864cd1c8d480402816cb15bcf
EL KAMEL Malek <elkamel_malek@hotmail.fr>
2016-03-18 00:02:42 -0400

    Fragments and Navigation Skeleton

--- app/src/main/java/com/mk/meetbuddies/MainActivity.java
+++ app/src/main/java/com/mk/meetbuddies/MainActivity.java
@@ -1,8 +1,11 @@
 package com.mk.meetbuddies;
 
+
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +15,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.mk.meetbuddies.fragments.BuddiesFragment;
+import com.mk.meetbuddies.fragments.CalendarFragment;
+import com.mk.meetbuddies.fragments.LocationsFragment;
+import com.mk.meetbuddies.fragments.MeetingsFragment;
+import com.mk.meetbuddies.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -79,20 +88,30 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment = null;
+        FragmentManager fragmentManager = getSupportFragmentManager();
 
         if (id == R.id.nav_profile) {
-
+            fragment = new ProfileFragment();
         } else if (id == R.id.nav_calendar) {
-
+            fragment = new CalendarFragment();
         } else if (id == R.id.nav_buddies) {
-
+            fragment = new BuddiesFragment();
         } else if (id == R.id.nav_locations) {
-
+            fragment = new LocationsFragment();
         } else if (id == R.id.nav_meeting) {
-
+            fragment = new MeetingsFragment();
         }
+        setTitle(item.getTitle());
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        fragmentManager.beginTransaction()
+                .replace(R.id.mainFragment, fragment)
+                .commit();
+
+        // Highlight the selected item, update the title, and close the drawer
+        // Highlight the selected item has been done by NavigationView
+        // menuItem.setChecked(true);
+         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
