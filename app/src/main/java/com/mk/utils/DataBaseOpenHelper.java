b5ad44f2d0036272802468ebff8be0c0e2b15b4f
Salim <salim7ariz@gmail.com>
2016-03-21 14:23:55 -0400

    Fix DBOH

--- app/src/main/java/com/mk/utils/DataBaseOpenHelper.java
+++ app/src/main/java/com/mk/utils/DataBaseOpenHelper.java
@@ -20,7 +20,7 @@ public class DataBaseOpenHelper extends SQLiteOpenHelper {
         // TODO Auto-generated method stub
         String req = "CREATE TABLE "
                 + TABLE_USER
-                + " (_id INTEGER PRIMARY KEY,name TEXT ,prename TEXT ,login TEXT, password TEXT, photo TEXT, adress TEXT, organisateur TEXT, pref1 TEXT, pref2 TEX, pref3 TEX, pref4 TEX, pref5 TEX);)";
+                + " (_id INTEGER PRIMARY KEY,name TEXT ,prename TEXT ,login TEXT, password TEXT, photo TEXT, adress TEXT, group TEXT, pref1 TEXT, pref2 TEXT, pref3 TEXT, pref4 TEXT, pref5 TEXT, organizer TEXT);)";
         db.execSQL(req);
     }
 
